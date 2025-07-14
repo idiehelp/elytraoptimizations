af5adc8eb46d8de7fdabfa80da48b5f93563e6c7
idiehelp <stefanbujoreanu75@gmail.com>
2025-07-14 12:30:39 +0300

    -made a LUT for sines and cosines which made a ~10% increase in performance

--- src/main/java/com/qkba/elytraoptimization/mixins/elytraMixin.java
+++ src/main/java/com/qkba/elytraoptimization/mixins/elytraMixin.java
@@ -12,7 +12,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import java.util.Optional;
 
 @Mixin(PlayerEntity.class)
